073944f65c23ccb19bb186ea6ff1bddbe8bf2a77
Ippei523 <65227036+Ippei523@users.noreply.github.com>
2020-07-10 00:23:26 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+$(function() {
+  $("a").click(function() {
+    var target = $($(this).attr("href")).offset().top;
+
+    target -= 70;
+
+    $("html, body").animate({scrollTop: target}, 500);
+  });
+
+  var current = 1;
+  var next = 2;
+  var count = $(".skill__ability").length;
+  
+  $("#nextBtn").click(function() {
+    $(".skill__ability:nth-child(" + current + ")").hide();
+    $(".skill__ability:nth-child(" + next + ")").show();
+    
+    current = next;
+    next = next++;
+    if(next > count) {
+      next = 1;
+    }
+  });
+});
